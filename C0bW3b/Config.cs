cbd2135d5017df9d7a9e426b370b6adf8bbb7d8f
Damlasky17 <129737232+Damlasky17@users.noreply.github.com>
2023-08-08 12:55:09 +0100

    update file

---
+++ C0bW3b/Config.cs
@@ -0,0 +1,91 @@
+using C0bW3b.Forms;
+using C0bW3b.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C0bW3b
+{
+    public class ConfigSystem
+    {
+        public static Config config = new Config();
+        public static void SaveConfig()
+        {
+            try
+            {
+                string content = JsonConvert.SerializeObject(config, Formatting.Indented);
+                File.WriteAllText("config.json", content);
+            }
+            catch (Exception ex) { MessageBox.Show(" FAILED TO SAVE CONFIG!!!\n" + ex.Message, "C0bW3b ~ Error", MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+        }
+
+        public static void LoadConfig()
+        {
+            try
+            {
+                if (!File.Exists("config.json"))
+                {
+                    // fix output format
+                    config.OutputFormat = new Dictionary<string, bool>()
+                    {
+                        { "Url", true },
+                        { "Dork", false },
+                        { "Matches", false },
+                        { "Match Count", true },
+                        { "User Agent", false },
+                        { "Proxy", false },
+                        { "Engine", false }
+                    };
+
+                    SaveConfig();
+                }
+
+                string content = File.ReadAllText("config.json");
+                config = JsonConvert.DeserializeObject<Config>(content);
+            }
+            catch (Exception ex) { MessageBox.Show(" FAILED TO LOAD CONFIG!!!\n" + ex.Message, "C0bW3b ~ Error", MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+        }
+    }
+
+    public class Config
+    {
+        // RUNNER TAB
+        public int Threads = 100;
+        public bool AllowDuplicates = false;
+        public bool LogFullUrl = false;
+        public int MinimumMatches = 1;
+        public bool RecursiveSearch = true;
+        public int RecursiveLimit = 2;
+        public int UrlLimit = 100;
+
+        // SETTINGS TAB
+        // > GENERAL
+        public bool UpdateThreadStatus = true;
+        public bool AutoSave = false;
+        public int AutoSaveInterval = 120;
+        public bool GarbageCollector = true;
+        public int CollectionInterval = 60;
+        public bool LogCollection = false;
+        public bool DiscordRPC = false;
+
+        // > OUTPUT
+        public Dictionary<string, bool> OutputFormat = new Dictionary<string, bool>();
+        public string Seperator = "|";
+
+        // > THEME
+        public Theme CurrentTheme = new Theme();
+
+        // ENGINES TAB
+        public List<SearchEngine> SearchEngines = new List<SearchEngine>();
+
+        // BLACKLIST TAB
+        public string Blacklist = "";
+    }
+}
